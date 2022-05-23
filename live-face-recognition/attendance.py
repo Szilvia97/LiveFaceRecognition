c8b1d5f5ad47e88b6ba1f50e78cc623698c80521
zozo <zoltan.szanto@ms.sapientia.ro>
2022-05-23 15:12:29 +0300

    Updated req, added attendance class

---
+++ live-face-recognition/attendance.py
@@ -0,0 +1,109 @@
+from math import dist
+import numpy as np
+from face_recognizer import FaceRecognizer
+from live_recognizer import LiveeeeRecognizer
+from camera_stream import CameraStream
+import cv2
+from pathlib import Path
+from camera_stream import CameraStream
+import logging.config
+from configparser import ConfigParser
+import threading
+
+
+class Attendance:
+    def __init__(self, config):
+        self.config = config
+
+        self.camera_streamer = CameraStream(config)
+        self.camera_streamer.start()
+
+        self.face_rec = FaceRecognizer(config)
+        self.live_rec = LiveeeeRecognizer(config)
+
+        self.started = False
+        self.thread = threading.Thread(target=self.update, name='WorkerThread', args=())
+        self.read_lock = threading.Lock()
+
+        self.height = self.config.getint('CAMERA_HEIGHT')
+        self.width = self.config.getint('CAMERA_WIDTH')
+        self.frame = np.zeros((self.height, self.width, 3), np.uint8)
+
+        self.face_list = []
+        self.live_list = []
+
+    def start(self):
+        if self.started:
+            return
+
+        self.started = True
+        self.thread.start()
+
+    def stop(self):
+        if self.started:
+            self.started = False
+
+            self.thread.join()
+
+    def update(self):
+        logging.info('Start')
+
+        while self.started:
+            frame = self.camera_streamer.get_latest_frame()
+
+            self.face_list = self.face_rec.process_frame(frame)
+            self.live_list = self.live_rec.process_frame(frame)
+
+            if self.face_list:
+                for face in self.face_list:
+                    for detection in self.live_list:
+                        face_cog = face.get_COG()
+                        detection_cog = detection.get_COG()
+
+                        # TODO: remove
+                        logging.debug(
+                            f"Face: {face.get_COG()}, Live: {detection.get_COG()}, Dist = {dist(face.get_COG(), detection.get_COG())}")
+
+                        # TODO: add correct number here
+                        if dist(face_cog, detection_cog) < 100:
+                            # TODO: draw if needed, see config
+                            if True:
+                                cv2.rectangle(frame, (face.left, face.top), (face.right, face.bottom), (0, 255, 0), 2)
+                                cv2.putText(frame, face.name + " - " + detection.text, (face.left + 6, face.bottom - 6),
+                                            cv2.FONT_HERSHEY_DUPLEX, 0.6,
+                                            (255, 0, 255), 1)
+
+            self.read_lock.acquire()
+            self.frame = frame
+            self.read_lock.release()
+
+    def get_latest_frame(self) -> np.array:
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
+
+def main():
+    # TODO add config
+    config_object = ConfigParser()
+    config_object.read(Path("config.ini"))
+    config = config_object["DEFAULT"]
+
+    # logging.config.fileConfig(Path("log_config.ini"))
+
+    attendance = Attendance(config)
+    attendance.start()
+
+    while True:
+        frame = attendance.get_latest_frame()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        cv2.imshow('Video', frame)
+
+    attendance.stop()
+
+
+if __name__ == '__main__':
+    main()
+
--- live-face-recognition/ui.py
+++ live-face-recognition/ui.py
@@ -1,58 +1,76 @@
 import PySimpleGUI as psg
 from firebase import firebase
 from datetime import datetime
+import logging.config
+from configparser import ConfigParser
+from pathlib import Path
+from attendance import Attendance
 
 
-psg.theme('LightGreen3')
-layout=[[psg.Text('Válaszd ki a tárgyat',size=(100, 1), font='Lucida',justification='left')],
-        [psg.Combo(['Szoftverteszteles','OOP', 'Szoftverfejlesztes','Diszkret matematika', 'Kriptografia','Webtechnologiak'],key='subject')],
-        [psg.Text('Válaszd ki az óra típusát ',size=(30, 1), font='Lucida',justification='left')],
-        [psg.Combo(['Eloadas','Labor'],key='type')],
-        [psg.Text('Válaszd ki a szakot ',size=(30, 1), font='Lucida',justification='left')],
-        [psg.Combo(['Szamitastechnika','Informatika','Tavkozles'],key='class')],
-        [psg.Text('Válaszd ki a hetet ',size=(30, 1), font='Lucida',justification='left')],
-        [psg.Combo(['het1','het2', 'het3', 'het4', 'het5'],key='week')],
-        [psg.Text('Válaszd ki a termet ',size=(30, 1), font='Lucida',justification='left')],
-        [psg.Combo(['114','312', '230'],key='classroom')],
-        # [psg.Text('Válaszd ki az időpontot',size=(30, 1), font='Lucida',justification='left')],
-        # [psg.Listbox(values=['8:00 - 9:50', '10:00 - 11:50', '12:30 - 14:20'], select_mode='extended', key='time', size=(30, 6))],
-        [psg.Button('Kezdés', font=('Times New Roman',12), size=(10, 1)),psg.Button('Leállítás', font=('Times New Roman',12),size=(10, 1))]]
+config_object = ConfigParser()
+config_object.read(Path("config.ini"))
+config = config_object["DEFAULT"]
 
-window =psg.Window('Jelenlét',layout, size=(360,360), auto_size_buttons=True, auto_size_text=True, resizable=True,finalize=True)
+attendance = Attendance(config)
 
 firebase = firebase.FirebaseApplication('https://studentdatas-2ea41.firebaseio.com/', None)
 
+psg.theme('LightGreen3')
+layout = [[psg.Text('Válaszd ki a tárgyat', size=(100, 1), font='Lucida', justification='left')],
+          [psg.Combo(['Szoftverteszteles', 'OOP', 'Szoftverfejlesztes', 'Diszkret matematika', 'Kriptografia',
+                      'Webtechnologiak'], key='subject')],
+          [psg.Text('Válaszd ki az óra típusát ', size=(30, 1), font='Lucida', justification='left')],
+          [psg.Combo(['Eloadas', 'Labor'], key='type')],
+          [psg.Text('Válaszd ki a szakot ', size=(30, 1), font='Lucida', justification='left')],
+          [psg.Combo(['Szamitastechnika', 'Informatika', 'Tavkozles'], key='class')],
+          [psg.Text('Válaszd ki a hetet ', size=(30, 1), font='Lucida', justification='left')],
+          [psg.Combo(['het1', 'het2', 'het3', 'het4', 'het5'], key='week')],
+          [psg.Text('Válaszd ki a termet ', size=(30, 1), font='Lucida', justification='left')],
+          [psg.Combo(['114', '312', '230'], key='classroom')],
+          # [psg.Text('Válaszd ki az időpontot',size=(30, 1), font='Lucida',justification='left')],
+          # [psg.Listbox(values=['8:00 - 9:50', '10:00 - 11:50', '12:30 - 14:20'], select_mode='extended', key='time', size=(30, 6))],
+          [psg.Button('Kezdés', font=('Times New Roman', 12), size=(10, 1)),
+           psg.Button('Leállítás', font=('Times New Roman', 12), size=(10, 1))]]
+
+# ide lesz a kep
+# [sg.Image(filename='', key='image_box', size=(self.height, self.width))],
+# img_bytes = cv2.imencode(".png", frame)[1].tobytes()
+# self.window["image_box"].update(data=img_bytes)
+
+
+window = psg.Window('Jelenlét', layout, size=(360, 360), auto_size_buttons=True, auto_size_text=True, resizable=True,
+                    finalize=True)
 
 while True:
-        event, values = window.read()
-        if event in ('Leállítás', None):
-            break           
-        if event == 'Kezdés':
-            # win.close()
-            now = datetime.now()
-            date_string = now.strftime("%Y-%m-%d")
-            time_string = now.strftime("%H:%M:%S")
+    event, values = window.read()
+    if event in ('Leállítás', None):
+        break
+    if event == 'Kezdés':
+        # attendance.start()
+        # win.close()
+        now = datetime.now()
+        date_string = now.strftime("%Y-%m-%d")
+        time_string = now.strftime("%H:%M:%S")
 
-            studentData =  {'deviceId': '', 
-                            'isAttendanceRecovery': '',
-                            'neptunId': '',
-                            'profile': '',
-                            'studentName': ''
-                            }  
-            
-            subject = values['subject']
-            type = values['type']
-            className = values['class']
-            week = values['week']
-            classroom = values['classroom']
-            
-            result = firebase.patch('Jelenlet/' + subject + '/' + type + '/' + className + '/' + week + '/' + classroom + '/' + date_string + '/' + time_string, studentData)  
-            print(result) 
-        
-            # psg.popup('Az óra adatai',      
-                # 'Az óra neve: '+ values['subject'] + '\nTípusa: '+ values['type'] +' \nIdőpont: ' + classTime[1:len(classTime)-1] +' \nSzak: ' + values['class'])
+        studentData = {'deviceId': '',
+                       'isAttendanceRecovery': '',
+                       'neptunId': '',
+                       'profile': '',
+                       'studentName': ''
+                       }
 
+        subject = values['subject']
+        type = values['type']
+        className = values['class']
+        week = values['week']
+        classroom = values['classroom']
 
+        result = firebase.patch(
+            'Jelenlet/' + subject + '/' + type + '/' + className + '/' + week + '/' + classroom + '/' + date_string + '/' + time_string,
+            studentData)
+        print(result)
 
-        # elif event == 'Download':
-            
+        # psg.popup('Az óra adatai',
+        # 'Az óra neve: '+ values['subject'] + '\nTípusa: '+ values['type'] +' \nIdőpont: ' + classTime[1:len(classTime)-1] +' \nSzak: ' + values['class'])
+
+    # elif event == 'Download':
